4f72278a82a343f170a81a46b904180d5807011d
Marek Knápek <knapek.mar@gmail.com>
2026-09-18 15:05:00 +0200

    Bash.

---
+++ playground/bash/build.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -x
+set -e
+mk_this_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+mk_src_dir="${mk_this_dir}"/../src
+mk_cp_dir="${mk_src_dir}"
+mk_out_dir="${mk_this_dir}"/../build/classes
+javac -d "${mk_out_dir}" -cp "${mk_src_dir}" "${mk_src_dir}"/mk/gui/frame.java
+javac -d "${mk_out_dir}" -cp "${mk_src_dir}" "${mk_src_dir}"/mk/gui/gui_starter.java
+javac -d "${mk_out_dir}" -cp "${mk_src_dir}" "${mk_src_dir}"/mk/gui/guiapp1.java
+javac -d "${mk_out_dir}" -cp "${mk_src_dir}" "${mk_src_dir}"/mk/jvm/info.java
+javac -d "${mk_out_dir}" -cp "${mk_src_dir}" "${mk_src_dir}"/mk/test/itest.java
+javac -d "${mk_out_dir}" -cp "${mk_src_dir}" "${mk_src_dir}"/mk/test/test.java
+javac -d "${mk_out_dir}" -cp "${mk_src_dir}" "${mk_src_dir}"/mk/uint/constants.java
+javac -d "${mk_out_dir}" -cp "${mk_src_dir}" "${mk_src_dir}"/mk/uint/u128.java
+javac -d "${mk_out_dir}" -cp "${mk_src_dir}" "${mk_src_dir}"/mk/uint/u16.java
+javac -d "${mk_out_dir}" -cp "${mk_src_dir}" "${mk_src_dir}"/mk/uint/u32.java
+javac -d "${mk_out_dir}" -cp "${mk_src_dir}" "${mk_src_dir}"/mk/uint/u64.java
+javac -d "${mk_out_dir}" -cp "${mk_src_dir}" "${mk_src_dir}"/test/mk/uint/base.java
+javac -d "${mk_out_dir}" -cp "${mk_src_dir}" "${mk_src_dir}"/test/mk/uint/common.java
+javac -d "${mk_out_dir}" -cp "${mk_src_dir}" "${mk_src_dir}"/test/mk/uint/test_all.java
+javac -d "${mk_out_dir}" -cp "${mk_src_dir}" "${mk_src_dir}"/test/mk/uint/test_u16.java
+javac -d "${mk_out_dir}" -cp "${mk_src_dir}" "${mk_src_dir}"/test/mk/uint/test_u32.java
---
+++ playground/bash/run_guiapp1.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -x
+set -e
+mk_this_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+mk_cp_dir="${mk_this_dir}"/../build/classes
+java -cp "${mk_cp_dir}" mk.gui.guiapp1
